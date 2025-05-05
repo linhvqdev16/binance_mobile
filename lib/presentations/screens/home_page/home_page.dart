b474d60f39c653c06015ee62c348251dcd0bed59
Ngoc <vuhoaingoc1608@gmail.com>
2025-05-05 15:31:32 +0700

    update

--- lib/presentations/screens/home_page/home_page.dart
+++ lib/presentations/screens/home_page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:binance_mobile/core/dependency_injection/injection_container.dart';
 import 'package:binance_mobile/presentations/screens/home_page/crypto_list_widget.dart';
+import 'package:binance_mobile/presentations/screens/market_page/marketDetail_page.dart';
 import 'package:binance_mobile/presentations/screens/market_page/market_Home.dart';
 import 'package:binance_mobile/presentations/screens/trade_page/trade_home.dart';
 import 'package:binance_mobile/presentations/screens/wallets_page/wallets_home.dart';
@@ -20,9 +21,7 @@ class MainPage extends ConsumerWidget {
       HomePage(),
       const MarketHome(),
       const TradeHome(),
-      const Center(
-        child: Text('Đây là màn Futures'),
-      ),
+      const MarketPageChart(),
       const WalletsHome()
     ];
 
@@ -544,65 +543,6 @@ class HomePage extends ConsumerWidget {
             fontWeight: FontWeight.w600,
           ),
           overflow: TextOverflow.ellipsis),
-    );
-  }
-
-  Widget _buildBottomNavigationBar() {
-    return BottomNavigationBar(
-      currentIndex: 0,
-      // onTap: (index) {
-      //   setState(() {
-      //     _currentIndex = index;
-      //   });
-      // },
-      elevation: 0,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.black,
-      unselectedItemColor: Colors.grey.withOpacity(0.5),
-      selectedLabelStyle: const TextStyle(fontSize: 12, color: Colors.black),
-      unselectedLabelStyle: const TextStyle(fontSize: 12),
-      items: [
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/icons/house.png',
-            width: 25,
-            height: 25,
-          ),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/icons/statistics.png',
-            width: 25,
-            height: 25,
-          ),
-          label: 'Markets',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/icons/trade.png',
-            width: 25,
-            height: 25,
-          ),
-          label: 'Trade',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/icons/future.png',
-            width: 25,
-            height: 25,
-          ),
-          label: 'Futures',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/icons/wallet.png',
-            width: 25,
-            height: 25,
-          ),
-          label: 'Wallets',
-        ),
-      ],
     );
   }
 }
